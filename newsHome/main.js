a49f1b7da01b3424589e4d8dd74bbbccb2d56bca
mohammedafsalk <techafs456@gmail.com>
2023-06-16 16:01:37 +0530

    nav close and open added

--- newsHome/main.js
+++ newsHome/main.js
@@ -1,1 +1,12 @@
 import './style.scss'
+
+let open = document.querySelector(".open")
+let close = document.querySelector(".close")
+
+close.addEventListener("click",()=>{
+    document.body.classList.remove("active")
+})
+
+open.addEventListener("click",()=>{
+    document.body.classList.add("active")
+})
